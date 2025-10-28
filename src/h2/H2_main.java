ebca22836a8aee409e2b041ada596181550540c1
Son <son.trinhh07@gmail.com>
2025-10-28 21:21:47 +0100

    3. Upload

---
+++ src/h2/H2_main.java
@@ -0,0 +1,27 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2, j = 3, k = 4, max = i, min = i;
+
+		if (j < min) {
+			min = j;
+		}
+		if (k < min) {
+			min = k;
+		}
+		if (i > max) {
+			max = i;
+		}
+		if (k > max) {
+			max = k;
+		}
+		System.out.println(min);
+		{
+		System.out.println(max);
+		}
+	}
+
+}
