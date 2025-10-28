45142fb554c633de77c6a5b386a24a92ef02a3df
Son <son.trinhh07@gmail.com>
2025-10-28 11:19:19 +0100

    2. Upload

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -3,14 +3,11 @@ package p2;
 public class P2_main {
 
 	public static void main(String[] args) {
-		int i=5;
-		boolean b=false;
-		if (i>10){
-			
-			b=true;
+		int i = 5;
+		boolean b = false;
+		if (i > 10) {
+			b = true;
 		}
 		System.out.println(b);
-
 	}
-
 }
---
+++ src/p3/P3_main.java
@@ -0,0 +1,19 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 2, j = 3, k = 4;
+		if (i < j) {
+			System.out.println("A");
+		}
+		if (i < k) {
+			System.out.println("B");
+		}
+
+		if (j < k) {
+			System.out.println("C");
+		}
+	}
+}
