9d5ddb5a139c81d738bb15dcd9992a9e9284fca9
Son <son.trinhh07@gmail.com>
2025-10-28 10:44:50 +0100

    2. Upload

---
+++ src/uebung2/p1_main.java
@@ -0,0 +1,17 @@
+package uebung2;
+
+import java.util.Scanner;
+
+public class p1_main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
+	
+
+	}
+
+}
